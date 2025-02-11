efe5753411248a9d3303a9bbee1cc9c8dae54983
Molean <cnmolean@gmail.com>
2025-02-11 13:34:32 +0800

    1

--- src/main/java/com/molean/folia/needs/mixin/poster/PlayerPostRespawnEventPoster.java
+++ src/main/java/com/molean/folia/needs/mixin/poster/PlayerPostRespawnEventPoster.java
@@ -2,24 +2,21 @@ package com.molean.folia.needs.mixin.poster;
 
 import ca.spottedleaf.concurrentutil.completable.CallbackCompletable;
 import com.destroystokyo.paper.event.player.PlayerPostRespawnEvent;
-import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.List;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ServerPlayer.class)
 public abstract class PlayerPostRespawnEventPoster {
-    @Shadow public abstract CraftPlayer getBukkitEntity();
+    @Shadow
+    public abstract CraftPlayer getBukkitEntity();
 
-    @Inject(method = "lambda$respawn$29", at = @At("TAIL"))
-    public void on(ServerLevel respawnWorld, BlockPos respawnPos, float respawnAngle, boolean isRespawnForced, boolean alive, CallbackCompletable spawnPosComplete, boolean[] usedRespawnAnchor, List chunks, CallbackInfo ci) {
+    @Redirect(method = "lambda$respawn$29", at = @At(value = "INVOKE", target = "Lca/spottedleaf/concurrentutil/completable/CallbackCompletable;complete(Ljava/lang/Object;)V"))
+    public void on(CallbackCompletable instance, Object result) {
+        instance.complete(result);
         new PlayerPostRespawnEvent(getBukkitEntity(), getBukkitEntity().getLocation(), false).callEvent();
     }
 }
